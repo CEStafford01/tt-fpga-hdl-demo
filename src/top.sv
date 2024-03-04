553a050a9b08859767c5fbaa5a988a65202312e4
CEStafford01 <161381577+CEStafford01@users.noreply.github.com>
2024-03-04 16:00:22 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -250,7 +250,7 @@ logic FpgaPins_Fpga_CALC_sel_a1;
                   //_@1
             
                      assign FpgaPins_Fpga_CALC_digit_a1[7:0] = ui_in;
-                     assign FpgaPins_Fpga_CALC_sel_a1 = >>4clk;
+                     assign FpgaPins_Fpga_CALC_sel_a1 = clk;
                      assign uo_out = (FpgaPins_Fpga_CALC_digit_a1[0] & FpgaPins_Fpga_CALC_sel_a1)
                                  ? 8'b00001111:
                                  (FpgaPins_Fpga_CALC_digit_a1[0] & !FpgaPins_Fpga_CALC_sel_a1)
