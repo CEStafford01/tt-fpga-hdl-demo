45c49297cc72defec513cf7ca910daf039508c07
CEStafford01 <161381577+CEStafford01@users.noreply.github.com>
2024-03-05 13:47:23 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -388,9 +388,9 @@ logic FpgaPins_Fpga_TRAFFIC_LightLt_rs_a1;
                                           FpgaPins_Fpga_TRAFFIC_cycle_a1 ?
                                           32'b0:
                                           FpgaPins_Fpga_TRAFFIC_second_counter_a1 +1;
-                     assign FpgaPins_Fpga_TRAFFIC_cycle_a0 = (FpgaPins_Fpga_TRAFFIC_second_counter_a0[31] & !FpgaPins_Fpga_TRAFFIC_second_counter_a1[31]);
+                     assign FpgaPins_Fpga_TRAFFIC_cycle_a0 = (FpgaPins_Fpga_TRAFFIC_second_counter_a0[31]);
                      assign FpgaPins_Fpga_TRAFFIC_sel_a0 = FpgaPins_Fpga_TRAFFIC_second_counter_a0[4];
-            
+            //& !FpgaPins_Fpga_TRAFFIC_second_counter_a1[31]
                   //_@1
                      assign FpgaPins_Fpga_TRAFFIC_ba_a1 = FpgaPins_Fpga_TRAFFIC_b_or_a_a1;
                      assign FpgaPins_Fpga_TRAFFIC_ab_a1 = !FpgaPins_Fpga_TRAFFIC_b_or_a_a1;
